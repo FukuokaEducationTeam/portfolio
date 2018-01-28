9c4985c111a9c2a21caecbfb3bd200199e480afa
katahira yuta <mgy.90.24.yu@gmail.com>
2018-01-28 19:03:46 +0900

    first commit

--- config/application.rb
+++ config/application.rb
@@ -2,17 +2,21 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
+
 Bundler.require(*Rails.groups)
 
 module Portfolio
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.test_framework  nil 
+      g.assets  false
+      g.helper false
+      g.stylesheets false
+    end
+    config.i18n.default_locale = :ja
+
+    config.time_zone = 'Tokyo'
   end
 end
